dc112d6b7c52fd2998fdd612d87a9c6838c46142
Nick Mc <nbmc@protonmail.com>
2020-02-03 09:38:55 -0600

    steps 1 through 3.

---
+++ scripts.js
@@ -0,0 +1,4 @@
+name = "Nick";
+var name;
+console.log(name);
+// I expected "Nick" to be logged because vars are hoisted: brought to the top of the document on load, so it actually is declared before assignment and logging.
