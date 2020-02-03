fe157999dc60ae86c992211f0c0db9c167acf68f
Nick Mc <nbmc@protonmail.com>
2020-02-03 13:19:06 -0600

    steps 9 through 11.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-name = "Nick";
+name = 'Nick';
 var name;
 // console.log(name);
 // after step 3: I expected "Nick" to be logged because vars are hoisted: brought to the top of the document on load, so it actually is declared before assignment and logging.
@@ -7,4 +7,12 @@ function setName() {
     var name = 'Covalence';
     console.log(name);
 }
-// after step : I expect "Nick" to be logged because it is a globally scoped variable and takes precedence over the locally scoped variable within the function.
+// after step 8: I expect "Nick" to be logged because it is a globally scoped variable and takes precedence over the locally scoped variable within the function.
+console.log('first text');
+let avg = avgPair(2, 4);
+console.log('second text');
+function avgPair(a, b) {
+    console.log('third text');
+    return (a + b) / 2;
+};
+// after step 11: I expect the order to be first, third, second. This is because the function (which contains 'third text') is invoked before the 'second text' log.
